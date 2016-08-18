a8a3ad9e9de542a18866b7ddd43627887c1f7791
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-08-18 15:13:23 +0530

    Added link animations

--- app/components/SideNav.js
+++ app/components/SideNav.js
@@ -10,11 +10,19 @@ export default class SideNav extends React.Component {
       <div id="outer-container">
         <Menu isOpen noOverlay customCrossIcon={ false } >
           <p id="menu-header"><img src="/img/logo_white.png"/>healthfront</p>
-          <a id="status" className="menu-item" href="/status"><i className="fa fa-fw fa-heart-o"></i>status</a>
-          <a id="activity" className="menu-item" href="/activity"><i className="fa fa-fw fa-child"></i>activity</a>
-          <a id="sleep" className="menu-item" href="/sleep"><i className="fa fa-fw fa-moon-o"></i>sleep</a>
-          <a id="tests" className="menu-item" href="/tests"><i className="fa fa-fw fa-file-text-o"></i>tests</a>
-          <p className="menu-footer">Built with <i className="fa fa-heart red"></i> by Aditya Srinivasan<br/>Source code available at <a href="https://github.com/as577/healthfrontx">GitHub</a></p>
+          <nav className="cl-effect-21" id="cl-effect-21">
+            <a id="status" className="menu-item" href="/status"><i className="fa fa-fw fa-heart-o"></i>status</a>
+          </nav>
+          <nav className="cl-effect-21" id="cl-effect-21">
+            <a id="activity" className="menu-item" href="/activity"><i className="fa fa-fw fa-child"></i>activity</a>
+          </nav>
+          <nav className="cl-effect-21" id="cl-effect-21">
+            <a id="sleep" className="menu-item" href="/sleep"><i className="fa fa-fw fa-moon-o"></i>sleep</a>
+          </nav>
+          <nav className="cl-effect-21" id="cl-effect-21">
+            <a id="tests" className="menu-item" href="/tests"><i className="fa fa-fw fa-file-text-o"></i>tests</a>
+          </nav>
+          <p className="menu-footer">Built with <i className="fa fa-heart red"></i> by <a href="http://adityasrinivasan.io">Aditya Srinivasan</a><br/>Source code available at <a href="https://github.com/as577/healthfrontx">GitHub</a></p>
       </Menu>
       </div>
     );
