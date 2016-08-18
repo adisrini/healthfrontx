a7a0b5b138db2b85734517c34e1a7e537ac5ae70
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-08-18 14:10:49 +0530

    Added margin to content elements

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Home extends React.Component {
 	render() {
     	return (
-      		<div className='alert alert-info'>
+      		<div className='alert alert-info content'>
         		Hello from Home Component...
       		</div>
     	);
--- app/components/SideNav.js
+++ app/components/SideNav.js
@@ -8,7 +8,7 @@ export default class SideNav extends React.Component {
   render() {
     return (
       <div id="outer-container">
-        <Menu isOpen noOverlay >
+        <Menu isOpen noOverlay customCrossIcon={ false } >
           <p id="menu-header"><img src="/img/logo_white.png"/>healthfront</p>
           <a id="status" className="menu-item" href="/status"><i className="fa fa-heart-o"></i>status</a>
           <a id="status" className="menu-item" href="/activity"><i className="fa fa-male"></i>&nbsp;&nbsp;activity</a>
