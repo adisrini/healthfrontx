84c74ab8d2792469987a2a45484ca651a29038d1
Aditya Srinivasan <asrinivasan@cinchapi.com>
2016-11-17 01:45:03 -0500

    Add sleep page

--- app/components/Sleep.js
+++ app/components/Sleep.js
@@ -5,17 +5,10 @@ export default class Sleep extends React.Component {
 
 	render() {
     	return (
-      		<div className='content'>
-						<Title title="Sleep" subtitle="Zzzzzz..." />
-						<div className="row">
-							<div className="col-md-6">
-								<h1>Hello</h1>
-							</div>
-							<div className="col-md-6">
-								<h1>World</h1>
-							</div>
-						</div>
-      		</div>
+				<div className='content'>
+					<Title title="Status" subtitle="Zzzzz..." />
+
+				</div>
     	);
   	}
 }
